b6af29500ece853a6c35a48c8e660d7217dd1aec
rrd108 <rrd@webmania.cc>
2024-12-06 12:51:56 +0100

    chore add vite

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import path from 'path'
+
+export default defineConfig({
+    plugins: [
+        dts({
+            rollupTypes: true,
+            outDir: 'dist'
+        })
+    ],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'simplepay-js-sdk',
+            fileName: 'index'
+        },
+        rollupOptions: {
+            output: [
+                {
+                    format: 'es',
+                    entryFileNames: '[name].js',
+                }
+            ]
+        }
+    }
+})
