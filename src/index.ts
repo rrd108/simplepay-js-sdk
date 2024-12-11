de3004504ba1a75c915ed36474b755306f561343
rrd108 <rrd@webmania.cc>
2024-12-11 17:00:55 +0100

    feat export types for users

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { startPayment } from './oneTime'
 import { startRecurringPayment, startTokenPayment} from './recurring'
+import type { Currency, Language, PaymentMethod } from './types'
 import { checkSignature, generateSignature, getPaymentResponse, toISO8601DateString } from './utils'
 
 export {
+    Currency, Language, PaymentMethod,
     checkSignature, generateSignature, toISO8601DateString, getPaymentResponse,
     startPayment,     
     startRecurringPayment, startTokenPayment,
