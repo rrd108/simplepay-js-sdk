fbde7e89a367717c86a552835af025337e0cd010
rrd108 <rrd@webmania.cc>
2026-01-20 16:29:47 +0100

    fix: export return types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { startPayment } from './oneTime'
 import { startRecurringPayment, startTokenPayment, cancelCard} from './recurring'
-import type { Currency, Language, PaymentMethod } from './types'
+import type { Currency, Language, PaymentMethod, SimplePayResponse, SimplePayRecurringResponse, SimplePayTokenResponse, SimplePayCancelCardResponse, SimplePayResult } from './types'
 import { checkSignature, generateSignature, getPaymentResponse, toISO8601DateString } from './utils'
 
 export {
     Currency, Language, PaymentMethod,
+    SimplePayResponse, SimplePayRecurringResponse, SimplePayTokenResponse, SimplePayCancelCardResponse, SimplePayResult,
     checkSignature, generateSignature, toISO8601DateString, getPaymentResponse,
     startPayment,     
     startRecurringPayment, startTokenPayment, cancelCard
